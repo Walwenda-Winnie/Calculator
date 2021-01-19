5f20e2efe27a7d8a6f53dbad9121a2d481bf9f68
Walwenda-Winnie <walwendawinnie@gmail.com>
2021-01-19 17:02:24 +0300

    Another commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,96 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent  implements OnInit {
   title = 'Application1';
+  constructor() {}
+  screen="";
+  a="";
+  b="";
+  c="";
+  d="";
+  e="";
+
+
+ ngOnInit(): void {
+ }
+ entervalue(value: string){
+
+  if((this.b=="+")||(this.b=="-")||(this.b=="*")||(this.b=="/"))
+  {
+
+
+
+
+
+    this.d=this.d+value;
+    this.screen=this.screen+value;
+    this.c=this.d;
+
+  }
+  else
+  {
+    this.screen=this.screen+value;
+ 
+ 
+ 
+  }
+
 }
+condition(value: string)
+{
+  this.screen=this.screen+value;
+  this.b=value;
+}
+clear()
+{
+  this.screen="";
+  this.a="";
+  this.b="";
+  this.c="";
+  this.d="";
+
+
+}
+
+objectArray=[2-36];
+
+
+result(){
+  if(this.b=="+")
+  {
+
+    this.screen=this.screen+"="+(parseInt(this.a)+parseInt(this.c)).toString();
+    this.screen=(parseInt(this.screen)+parseInt(this.c)).toString();
+
+    this.objectArray.push(parseInt(this.screen)+parseInt(this.c));
+    console.log(this.objectArray)
+
+  }
+
+  if(this.b=="-")
+  {
+    this.screen=this.screen+"="+(parseInt(this.a)-parseInt(this.c)).toString();
+
+    this.screen=(parseInt(this.screen)-parseInt(this.c)).toString();
+
+  }
+  if(this.b=="*"){
+    this.screen=this.screen+"="+(parseInt(this.a)*parseInt(this.c)).toString();
+    this.screen=(parseInt(this.screen)*parseInt(this.c)).toString();
+ }
+ if(this.b=="/"){
+   this.screen=this.screen+"="+(parseInt(this.a)/parseInt(this.c)).toString();
+
+ }
+}
+
+
+
+}
+
+
